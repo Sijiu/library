f4d360753d1879bf1517c3ac4eb64172fb58b839
Plain <mxh403@163.com>
2016-07-12 22:47:04 +0800

    Natural Language Processing

---
+++ nltk_study/concordance.py
@@ -0,0 +1,10 @@
+# --coding utf-8
+from nltk.book import *
+
+text1.concordance("monstrous")
+print "-"*9
+# text2.concordance("monstrous")
+# text1.similar("monstrous")
+# text1.similar("imperial")
+# text1.common_contexts(["monstrous", "imperial"])
+# text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
